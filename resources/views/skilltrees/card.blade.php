19ef331e7af738861d141829c17611eb6b191222
JensAndreasson <42906632+JensAndreasson@users.noreply.github.com>
2019-06-20 10:24:40 +0200

    logo, teacher bool, avatars card

--- resources/views/skilltrees/card.blade.php
+++ resources/views/skilltrees/card.blade.php
@@ -1,7 +1,18 @@
 <div class="card shadow-sm bg-white my-2" style="width: 18rem;">
+    <h5 class="card-header"><a href="{{ $skilltree->path() }}">{{ $skilltree->title }}</a></h5>
     <div class="card-body">
-        <h5 class="card-title border-bottom"><a href="{{ $skilltree->path() }}">{{ $skilltree->title }}</a></h5>
         <p class="card-text text-muted">{{ str_limit($skilltree->description, 150) }}</p>
+    </div>
+    <div class="card-footer d-flex justify-content-between">
+        <div>
+            @foreach ($skilltree->members as $member)
+                <img
+                    src="{{ $member->avatar }}"
+                    alt="{{ $member->name }}'s avatar"
+                    class="rounded-circle mr-1"
+                    style="width: 24px;">
+            @endforeach
+        </div>
         @can('manage', $skilltree)
             <form action="{{ $skilltree->path() }}" method="post">
                 @csrf
