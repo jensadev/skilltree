40ce72773c6fc6364c6c8376a0aa04af9892a7cf
JensAndreasson <jensandreasson77@gmail.com>
2019-06-27 18:23:59 +0200

    loging page noodling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,22 +3,28 @@
 @section('content')
 <header class="loginheader w-100 ptop bg-dark">
     <div class="container">
-        <div class="loginheader-border">
-            <div class="loginheader-left"></div>
-            <h1 class="h1 text-uppercase text-center display-2 text-light loginheader-text">Skilltree</h1>
+        <div class="row">
+            <div class="col-lg-8 loginheader-border">
+                <div class="loginheader-left"></div>
+                <h1 class="h1 text-uppercase display-2 text-light loginheader-text">Skilltree</h1>
+            </div>
         </div>
-        <div class="loginheader-content text-light">
-            <h2 class="h3">This tool</h2>
-            <p class="lead">Är great! Lorem ipsum dolo.... och behöver linjering...</p>
+        <div class="row">
+            <div class="col-lg-8 text-light pt-5 loginheader-padding" style="padding-bottom: 100px;">
+                <h2 class="h3">This tool</h2>
+                <p class="lead">Är great! Lorem ipsum dolo.... och behöver linjering...</p>
+            </div>
         </div>
     </div>
 </header>
 <div class="container ptop">
-    <div class="" style="width:600px; padding-left: 70px;">
+    <div class="row">
+        <div class="col-lg-6 loginheader-padding">
         <a href="{{ route('login.provider', 'google') }}"
                     class="btn btn-lg btn-light bg-white shadow-lg">
                     <img src="/img/google_g_logo.svg">
                 {{ __('Google Sign in') }}</a>
+        </div>
     </div>
 </div>
 @endsection
