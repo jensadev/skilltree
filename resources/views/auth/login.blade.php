41d593e7c7086d8ef3dd74923b2d4c5c43886f2d
JensAndreasson <jensandreasson77@gmail.com>
2019-06-27 17:53:20 +0200

    important loginpage styling that breaks on phones...weee

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 <header class="loginheader w-100 ptop bg-dark">
     <div class="container">
-        <h1 class="h1 text-uppercase display-2 text-light">Skilltree</h1>
+        <div class="loginheader-border">
+            <div class="loginheader-left"></div>
+            <h1 class="h1 text-uppercase text-center display-2 text-light loginheader-text">Skilltree</h1>
+        </div>
+        <div class="loginheader-content text-light">
+            <h2 class="h3">This tool</h2>
+            <p class="lead">Är great! Lorem ipsum dolo.... och behöver linjering...</p>
+        </div>
     </div>
 </header>
 <div class="container ptop">
-    <div class="row justify-content-center">
-        <div class="col-8">
-            <a href="{{ route('login.provider', 'google') }}"
-                        class="btn btn-lg btn-light bg-white shadow-lg">
-                        <img src="/img/google_g_logo.svg">
-                    {{ __('Google Sign in') }}</a>
-        </div>
+    <div class="" style="width:600px; padding-left: 70px;">
+        <a href="{{ route('login.provider', 'google') }}"
+                    class="btn btn-lg btn-light bg-white shadow-lg">
+                    <img src="/img/google_g_logo.svg">
+                {{ __('Google Sign in') }}</a>
     </div>
 </div>
 @endsection
